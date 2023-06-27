ab70300f221d763b80085f0e1e452832144375b7
Tarun <tarunsrisaiv03@gmail.com>
2023-06-27 07:04:01 +0530

    Fix indents

--- src/model.py
+++ src/model.py
@@ -34,7 +34,8 @@ def create_encodings(column, price):
     df_temp = pd.DataFrame({'column': column, 'price': price})
     unique_categories = df_temp['column'].unique()
     avg_prices = df_temp.groupby('column')['price'].mean()
-    sorted_categories = list(sorted(unique_categories, key=lambda x: avg_prices[x]))
+    sorted_categories = list(sorted(unique_categories,
+                                    key=lambda x: avg_prices[x]))
     encodings = {
         category: index for index,
         category in enumerate(sorted_categories)
@@ -56,27 +57,41 @@ def main():
 
     # Remove unwanted columns and rename
     df = df.drop('Unnamed: 0', axis=1)
-    df.insert(0, 'seller_name', df['Seller Name'].apply(lambda x: x.strip()))
+
+    df.insert(0, 'seller_name',
+        df['Seller Name'].apply(lambda x: x.strip()))
     df = df.drop('Seller Name', axis=1)
-    df.insert(1, 'seller_type', df['Seller type'].apply(lambda x: x.strip()))
+
+    df.insert(1, 'seller_type',
+        df['Seller type'].apply(lambda x: x.strip()))
     df = df.drop('Seller type', axis=1)
+
     df.insert(2, 'bhk', df['BHK'].apply(lambda x: x.strip()))
     df = df.drop('BHK', axis=1)
+
     df.insert(3, 'location', df['Location'].apply(lambda x: x.strip()))
     df = df.drop('Location', axis=1)
+
     df.insert(4, 'city', df['City'].apply(lambda x: x.strip()))
     df = df.drop('City', axis=1)
-    df.insert(5, 'price_per_sqft', df['price per sqft'].apply(lambda x: x.strip()))
+
+    df.insert(5, 'price_per_sqft',
+        df['price per sqft'].apply(lambda x: x.strip()))
     df = df.drop('price per sqft', axis=1)
+
     df.insert(6, 'area', df['Area_sqft'])
     df = df.drop('Area_sqft', axis=1)
-    df.insert(7, 'construction_status', df['Construction status'].apply(lambda x: x.strip()))
+
+    df.insert(7, 'construction_status',
+        df['Construction status'].apply(lambda x: x.strip()))
     df = df.drop('Construction status', axis=1)
+
     df.insert(8, 'price', df['Total Price'].apply(lambda x: x.strip()))
     df = df.drop('Total Price', axis=1)
 
     # Extracting plot type from BHK
     df.insert(3, 'plot_type', df['bhk'].apply(get_plot_type))
+
     df.insert(4, 'bhk/rk', df['bhk'].apply(get_bhk))
     df = df.drop(['bhk'], axis=1)
 
@@ -93,6 +108,7 @@ def main():
     df_num = pd.DataFrame()
     for column in encoding_variables:
         df_num[column] = encode(df[column], encodings[column])
+
     df_num.insert(3, 'bhk/rk', df['bhk/rk'])
     df_num.insert(6, 'area', df['area'])
 
@@ -101,12 +117,14 @@ def main():
 
     # Removing outliers
     value_counts = df_num['price'].value_counts().to_dict()
-    df_fil = df_num[df_num['price'].apply(lambda x: value_counts[x] > 10)]
+    df_fil = df_num[df_num['price'].apply(
+        lambda x: value_counts[x] > 10)]
 
     # Model training
     X = df_fil.iloc[:, 2:-1]
     Y = df_fil.iloc[:, -1]
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, test_size=0.3, random_state=42)
     model = LinearRegression()
     model.fit(X_train, Y_train)
 
@@ -117,7 +135,8 @@ def main():
             'df_dict': df.to_dict(),
             'columns': X.columns.tolist()
         }, open(os.path.join('..', 'input', 'input.json'), 'w'), indent=4)
-    pk.dump(model, open(os.path.join('..', 'models', 'linear_regression.sav'), 'wb'))
+    pk.dump(model, open(os.path.join(
+        '..', 'models', 'linear_regression.sav'), 'wb'))
 
 
 if __name__ == "__main__":
