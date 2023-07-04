90b37f3afe2b06285593baf5b9e0cfdcc9541cfe
Tarun <tarunsrisaiv03@gmail.com>
2023-07-04 19:46:23 +0530

    Remove semicolon

--- real-estate-predictor/src/app/app.module.ts
+++ real-estate-predictor/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
 import { NgSelectModule } from '@ng-select/ng-select'
 
-import { AppComponent } from './app.component';
-import { TitleComponent } from './title/title.component';
-import { ResultComponent } from './result/result.component';
+import { AppComponent } from './app.component'
+import { TitleComponent } from './title/title.component'
+import { ResultComponent } from './result/result.component'
 import { InputComponent } from './input/input.component'
 
 @NgModule({
