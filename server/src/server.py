39d6290b74cada20013b59937e574a043138c8d2
Tarun <tarunsrisaiv03@gmail.com>
2023-07-10 08:38:36 +0530

    Add real_estate_predictor to endpoints

--- server/src/server.py
+++ server/src/server.py
@@ -9,19 +9,19 @@ fc.CORS(flask_app)
 real_app = App()
 
 
-@flask_app.route('/get_column_names', methods=['GET'])
+@flask_app.route('/real_estate_predictor/get_column_names', methods=['GET'])
 def get_column_names_endpoint():
     column_names = real_app.get_columns()
     return {'column_names': column_names}
 
 
-@flask_app.route('/get_data_values', methods=['GET'])
+@flask_app.route('/real_estate_predictor/get_data_values', methods=['GET'])
 def get_data_values_endpoint():
     data_values = real_app.get_data_values()
     return {'data_values': data_values}
 
 
-@flask_app.route('/process_input', methods=['PUT', 'GET'])
+@flask_app.route('/real_estate_predictor/process_input', methods=['PUT', 'GET'])
 def process_input_endpoint():
     method = fl.request.method
 
@@ -36,7 +36,7 @@ def process_input_endpoint():
             return {'processed_input': processed_input}
 
 
-@flask_app.route('/predict', methods=['PUT', 'GET'])
+@flask_app.route('/real_estate_predictor/predict', methods=['PUT', 'GET'])
 def predict_endpoint():
     method = fl.request.method
     
