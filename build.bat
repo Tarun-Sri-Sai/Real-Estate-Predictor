9bc39f95b006d33d0f8c6df24e63614e2aef3119
Tarun <tarunsrisaiv03@gmail.com>
2023-08-05 21:03:06 +0530

    Remove --force

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 
 start cmd /k "pip install pandas scikit-learn flask flask-cors & exit"
-start cmd /k "cd real-estate-predictor & npm install & npm install @angular/cli & npm audit fix --force & exit"
+start cmd /k "cd real-estate-predictor & npm install & npm install @angular/cli & npm audit fix & exit"
