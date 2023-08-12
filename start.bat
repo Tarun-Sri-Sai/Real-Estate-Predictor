ab0e023d26f469c9cbcae6501dbea74ea2579ace
Tarun <tarunsrisaiv03@gmail.com>
2023-08-12 17:16:34 +0530

    Wrap commands along multiple lines

--- start.bat
+++ start.bat
@@ -1,4 +1,8 @@
 @echo off
 
-start cmd /k "cd server/src & python server.py"
-start cmd /k "cd real-estate-predictor & npm start"
+start cmd /k ^
+    "cd server/src" ^
+    "& python server.py"
+start cmd /k ^
+    "cd real-estate-predictor" ^
+    "& npm start"
